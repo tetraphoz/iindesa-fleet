5d5269b5cfef2cce9ba951125bcd155b0c988cdd
tetraphoz <tetraphosphorus@gmail.com>
2026-09-22 20:20:05 -0600

    Use NixOS 26.05

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   description = "NixOS fleet configuration for the work ThinkPads";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs = { nixpkgs, ... }:
     let
       system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
       mkHost = { user, modules }:
         nixpkgs.lib.nixosSystem {
           inherit system;
@@ -32,5 +33,13 @@
           modules = [ ./hosts/x1-9thgen/configuration.nix ];
         };
       };
+
+      checks.${system}.inventory-script = pkgs.runCommand "check-nixos-inventory-script" {
+        nativeBuildInputs = [ pkgs.bash pkgs.shellcheck ];
+      } ''
+        bash -n ${./nixos-inventory.sh}
+        shellcheck ${./nixos-inventory.sh}
+        touch $out
+      '';
     };
 }
